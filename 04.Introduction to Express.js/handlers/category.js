5951890ee2bd95843fe25a6fac3d3d28d05d9e7a
IvanKrivulev <vande9855@gmail.com>
2018-06-14 13:47:35 +0300

    started shop stop part 4

---
+++ 04.Introduction to Express.js/handlers/category.js
@@ -0,0 +1,17 @@
+const url = require('url');
+const fs = require('fs');
+const path = require('path');
+const qs = require('querystring');
+
+const Category = require('../models/Category');
+
+module.exports.addGet = (req, res) => {
+    res.render('category/add');
+};
+
+module.exports.addPost = (req, res) => {
+    let category = req.body;
+    Category.create(category).then(() => {
+        res.redirect('/');
+    })
+};
